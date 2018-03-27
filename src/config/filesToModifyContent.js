3545b030de4d56917fd24bf0cf34bd5b8390d236
Felipe Leite <felipe.leite.gv@gmail.com>
2018-03-27 14:08:49 -0300

    Fix.

--- src/config/filesToModifyContent.js
+++ src/config/filesToModifyContent.js
@@ -3,7 +3,6 @@
 
 export function filesToModifyContent(currentAppName, newName, newProjectName) {
   const nS_CurrentAppName = currentAppName.replace(/\s/g, '');
-  const nS_NewName = newName.replace(/\s/g, '');
   const nS_NewProjectName = newProjectName.replace(/\s/g, '');
 
   return [
@@ -21,8 +20,8 @@ export function filesToModifyContent(currentAppName, newName, newProjectName) {
         'index.ios.js',
         `ios/${nS_NewProjectName}.xcodeproj/project.pbxproj`,
         `ios/${nS_NewProjectName}.xcworkspace/contents.xcworkspacedata`,
-        `ios/${nS_NewProjectName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}-tvOS.xcscheme`,
-        `ios/${nS_NewProjectName}.xcodeproj/xcshareddata/xcschemes/${nS_NewName}.xcscheme`,
+        `ios/${nS_NewProjectName}.xcodeproj/xcshareddata/xcschemes/${nS_NewProjectName}-tvOS.xcscheme`,
+        `ios/${nS_NewProjectName}.xcodeproj/xcshareddata/xcschemes/${nS_NewProjectName}.xcscheme`,
         `ios/${nS_NewProjectName}/AppDelegate.m`,
         'android/settings.gradle',
         `ios/${nS_NewProjectName}Tests/${nS_NewProjectName}Tests.m`,
